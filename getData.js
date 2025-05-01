59d8d2489084cb73fdab72d33234234256746bf4
Richard Barnes <rlb@ipv.sx>
2025-05-01 15:32:45 -0400

    Initial commit

---
+++ getData.js
@@ -0,0 +1,120 @@
+// Reverse engineered from https://apply.joinsherpa.com/map
+
+const AWT_API = "https://awt.cbp.gov/api/waitTime";
+const MS_PER_DAY = 24 * 60 * 60 * 1000;
+const MS_PER_YEAR = 365 * MS_PER_DAY;
+const MOVING_AVERAGE_DAYS = 30;
+const MOVING_AVERAGE_MS = MOVING_AVERAGE_DAYS * MS_PER_DAY;
+
+function uniq(arr) {
+  const uniq = arr.filter((val, i, arr) => arr.indexOf(val) === i);
+  uniq.sort();
+  return uniq;
+}
+
+function toISO(date) {
+  const yyyy = date.getFullYear().toString().padStart(4, 0);
+  const mm = (date.getMonth() + 1).toString().padStart(2, 0);
+  const dd = date.getDate().toString().padStart(2, 0);
+  return `${yyyy}-${mm}-${dd}`;
+}
+
+async function loadAirport(code, fromDate, toDate) {
+  const fromStr = toISO(fromDate);
+  const toStr = toISO(toDate);
+
+  const requestBody = `{
+    "Id":"${code}",
+    "FromDate":"${fromStr}",
+    "ToDate":"${toStr}"
+  }`;
+  
+  const response = await fetch(AWT_API, {
+    method: "POST",
+    body: requestBody,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const responseBody = await response.json();
+  const rows = responseBody.Items;
+  
+  // Compute totals per day
+  let totalNonUS = {};
+  for (let row of rows) {
+    totalNonUS[row["FlightDate"]] = totalNonUS[row["FlightDate"]] || 0;
+    totalNonUS[row["FlightDate"]] += row["NonUsaPassengerCount"];
+  }
+
+  // Compute 30-day moving average
+  const dates = uniq(rows.map(row => row["FlightDate"]));
+
+  let movingAverage = {};
+  for (let date of dates) {
+    let range = dates.filter(d => {
+      let diff = (new Date(date) - new Date(d));
+      return diff >= 0 && diff < MOVING_AVERAGE_MS;
+    });
+    if (range.length < MOVING_AVERAGE_DAYS) {
+      // console.error("Insufficient moving average data", date, range.length);
+      continue;
+    }
+
+    movingAverage[date] = range.map(d => totalNonUS[d]).reduce((a, b) => a + b);
+    movingAverage[date] /= range.length;
+  }
+  
+  // Compute year-on-year change
+  let yearOnYear = {};
+  for (let date of dates) {
+    let yearAgoCandidates = dates.filter(d => {
+      let dateDiffMS = (new Date(date) - new Date(d));
+      let yearDiffMS = dateDiffMS - MS_PER_YEAR;
+      return Math.abs(yearDiffMS) < MS_PER_DAY;
+    });
+    if (yearAgoCandidates.length == 0) {
+      // console.error("no year ago", date);
+      continue;
+    }
+
+    let yearAgo = yearAgoCandidates[0];
+    if (!movingAverage[yearAgo]) {
+      // console.error("no year ago data", date);
+      continue;
+    }
+
+    yearOnYear[date] = movingAverage[date] / movingAverage[yearAgo] - 1;
+  }
+
+  return yearOnYear;
+}
+
+const airports = [
+  "JFK",
+  "MIA",
+  "EWR",
+  "LAX",
+  "ORD",
+  "HNL",
+];
+
+const end = new Date();
+const start = new Date(end - (2 * 365 + MOVING_AVERAGE_DAYS) * MS_PER_DAY);
+
+const datasets = {};
+for (let code of airports) {
+  console.error(`${code} ...`);
+  datasets[code] = await loadAirport(code, start, end);
+}
+
+const dates = uniq(Object.keys(datasets).map(code => Object.keys(datasets[code])).flat());
+
+const data = dates.map(date => {
+  let nonUS = {};
+  for (let code in datasets) {
+    nonUS[code] = datasets[code][date];
+  }
+
+  return { date: toISO(new Date(date)), nonUS };
+});
+
+console.log(`const data = ${JSON.stringify(data, null, 2)};`);
